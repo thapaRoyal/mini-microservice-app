a46a98cd18139101a4ca303c3866320b99fcbb82
thapaRoyal <thaparoyal17@gmail.com>
2023-02-26 11:00:12 -0500

    parsing incoming events

--- query/index.js
+++ query/index.js
@@ -6,8 +6,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
-app.post('/events', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title };
+  }
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
