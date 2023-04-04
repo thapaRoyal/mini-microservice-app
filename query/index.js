10a2e792a51faaa58d517b4deaa1c0ea17da6fb5
thapaRoyal <thaparoyal17@gmail.com>
2023-04-04 21:07:09 +0545

    !OK

--- query/index.js
+++ query/index.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const posts = {};
 
 const handleEvent = (type, data) => {
@@ -50,7 +51,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Listening on 4002");
   try {
-    const res = await axios.get("http://localhost:4005/events");
+    const res = await axios.get("http://event-bus-srv:4005/events");
 
     for (let event of res.data) {
       console.log("Processing event:", event.type);
